8e98f3eddc38a7b9a55c16cca1d7e04e49fe9d5f
MoyStasik <73117395+MoyStasik@users.noreply.github.com>
2022-07-28 19:59:01 +0500

    Delete 8.1.py

--- 8.1.py
+++
@@ -1,7 +0,0 @@
-from itertools import product
-k = 0
-for x in product("012345", repeat = 6):
-    x = "".join(x)
-    if (x[0] != "0" and (x.count("0") + x.count("2") + x.count("4") == 2)):
-        k += 1
-print(k)
